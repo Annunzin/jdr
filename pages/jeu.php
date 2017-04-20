d7c8d758592db29c1912487ab63137ff664ffa4c
Annunzin <nicolas.annunziata@iut-valence.fr>
2017-04-20 13:13:54 +0100

    Create jeu.php

---
+++ pages/jeu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>JDR - Jeu</title>
+    <link rel="stylesheet" href="pages/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="pages/style/style.css">
+</head>
+<body>
+
+
+
+    <div class="container">
+
+
+
+
+
+
+        <h1><!-- Partie -->en cours...
+
+            <!-- Faire un retour avec sauvegarde et non un retour comme ça-->
+            &nbsp;&nbsp;<a class="btn btn-default" href="/jdr/"><span class="glyphicon glyphicon-home"></span></a>
+
+
+        </h1>
+        <hr/>
+
+
+    </div>
+
+
+
+<script src="pages/lib/jquery/jquery.min.js"></script>
+<script src="pages/lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
